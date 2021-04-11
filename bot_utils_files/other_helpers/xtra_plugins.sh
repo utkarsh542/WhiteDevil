94a69d713f4293fb2f407a974e8fbdffc9fb0cab
WhiteDevil-OP <82260005+WhiteDevil-OP@users.noreply.github.com>
2021-04-11 12:51:33 +0530

    Update xtra_plugins.sh

--- bot_utils_files/other_helpers/xtra_plugins.sh
+++ bot_utils_files/other_helpers/xtra_plugins.sh
@@ -6,7 +6,7 @@
 #
 # All rights reserved.
 
-PLUGIN_REPO="https://github.com/DevsExpo/Xtra-Plugins.git"
+PLUGIN_REPO="https://github.com/WhiteDevil-OP/WhiteDevilAddOn"
 xtra_fold="./xtraplugins"
 req_file="./xtraplugins/req.txt"
 
