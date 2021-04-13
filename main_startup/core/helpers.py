9604dcc5c38e81cf4547aef73f88bac4b83582f8
WhiteDevil-OP <82260005+WhiteDevil-OP@users.noreply.github.com>
2021-04-13 12:21:33 +0530

    Update helpers.py

--- main_startup/core/helpers.py
+++ main_startup/core/helpers.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2020-2021 by DevsExpo@Github, < https://github.com/DevsExpo >.
 #
-# This file is part of < https://github.com/DevsExpo/FridayUserBot > project,
+# This file is part of < https://github.com/WhiteDevil-OP/WhiteDevil-X > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/DevsExpo/blob/master/LICENSE >
 #
